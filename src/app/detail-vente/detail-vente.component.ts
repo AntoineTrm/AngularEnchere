e08435a39b91a36cedbec6abdbbbc1a575330371
ackoybella.leckey2022@campus-eni.fr <ackoybella.leckey2022@campus-eni.fr>
2022-09-05 11:13:33 +0200

    fichier vente-component.html et vente-component.ts modifier

--- src/app/detail-vente/detail-vente.component.ts
+++ src/app/detail-vente/detail-vente.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailVenteComponent implements OnInit {
 
+  detail={
+    img:"remplacé par une photo",
+    description:"Je suis l'outil de travail incontournable",
+    categorie:"informatique",
+    meilleure_offre:"210 points",
+    mise_a_prix:"185 point",
+    fin_de_enchere:"09/10/2018",
+    retrait:"10 allée des Alouettes 44800 Saint Herblain",
+    vendeur:"jojo44"
+    
+  }
+
   constructor() { }
 
   ngOnInit(): void {
